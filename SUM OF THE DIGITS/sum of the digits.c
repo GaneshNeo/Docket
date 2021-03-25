0baa2729000d1482e987e45e5dc8b7e9b9b72c48
GaneshNeo <1325982ganesh@gmail.com>
2021-03-25 21:10:45 +0530

    First commit

--- SUM OF THE DIGITS/sum of the digits.c
+++
@@ -1,3 +0,0 @@
-// check a number is perfect square or not
-// program to to compare 2 string withput strcmp
-// program to find matrix multiplication
--- hello world/hello world.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int n;  
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    printf("The number is : %d\n",n);
-    return 0;
-}
