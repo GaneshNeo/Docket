c0e145615a9273b6e24b7f52b8617461e2a6ba36
GaneshNeo <1325982ganesh@gmail.com>
2021-03-26 11:17:53 +0530

    strings

--- Reverse string preserving positions/Reverse_string_preserving_positions.c
+++ Reverse string preserving positions/Reverse_string_preserving_positions.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int i
+    int i,j;
+    char str[50],temp[50],res[50];
+    
+    printf("\nEnter a string : ");
+    scanf("%[^\n]%*c",str);
+
+    printf("\n%s\n",str);
+    return 0;
 }
