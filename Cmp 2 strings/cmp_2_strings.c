28fc8f795f9084b5eb2a1440411891450fd4f870
GaneshNeo <1325982ganesh@gmail.com>
2021-03-28 16:36:13 +0530

    Strings

--- Cmp 2 strings/cmp_2_strings.c
+++ Cmp 2 strings/cmp_2_strings.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
-#include<string.h>
+#include<ctype.h>
+
 int cmp_strings(char a[],char b[])
 {
-    printf("%s %s\n",a,b);
-    printf("%d %d\n",sizeof(a),sizeof(b));
     int i,flag=0;
     char m,n;
+
     for(i=0; (a[i] !='\0') && (b[i] !='\0');i++)
     {
-            printf("%c %c\n",a[i],b[i]);
-
-            m=strlwr(a[i]);
-            n=strlwr(b[i]);
-
-            printf("%c %c\n\n",a[i],b[i]);
-            if(n != m)
+            if(tolower(a[i]) != tolower(b[i]))
             {
                 flag=1;
             }
@@ -28,17 +22,24 @@ int cmp_strings(char a[],char b[])
         printf("Strings are not Equal\n");
     }
 }
+
 int main()
 {
     int i,n;
     char a[20],b[20];
+    
+    printf("\n\nEnter no of Testcases : ");
     scanf("%d\n",&n);
+
     for(i=0;i<n;i++)
     {
-        scanf("%s %s",a,b);
-        printf("%s %s\n",a,b);
-        printf("%d %d\n",sizeof(a),sizeof(b));
+        printf("Enter the First string : ");
+        scanf("%s\n",a);
+        printf("Enter the Second string : ");
+        scanf("%s\n",b);
+        
         cmp_strings(a,b);
     }
+    printf("\n\n");
     return 0;
 }
