06af51c483c45a2c7c8cddf7148ff75dc1c2445a
GaneshNeo <1325982ganesh@gmail.com>
2021-03-28 16:19:39 +0530

    Loops

--- Binary Addition/Binary_addition.c
+++ Binary Addition/Binary_addition.c
@@ -4,8 +4,13 @@ int main()
 {
     long b1,b2;
     int i=0,rem=0,sum[20];
+
+    printf("\n\nBinary Addition....\n");
+    printf("Enter First binary Number : ");
     scanf("%ld", &b1);
+    printf("Enter Second binary Number : ");
     scanf("%ld", &b2);
+    
     while(b1 != 0 || b2 != 0)
     {
         sum[i++] =(b1 % 10 + b2 % 10 + rem) % 2;
@@ -13,11 +18,15 @@ int main()
         b1 = b1 / 10;
         b2 = b2 / 10;
     }
-    if (rem != 0)
+    if (rem != 0) {
         sum[i++] = rem;
+    }
     --i;
-    printf("Sum of two binary numbers: ");
+    
+    printf("Sum of two binary numbers : ");
+    
     while (i >= 0)
         printf("%d", sum[i--]);
+    printf("\n\n");
     return 0;
 }
