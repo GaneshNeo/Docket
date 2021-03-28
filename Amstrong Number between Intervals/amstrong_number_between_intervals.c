cc74be2d01dccb87f70fa7fa6fcc42bfafd6434d
GaneshNeo <1325982ganesh@gmail.com>
2021-03-28 16:14:27 +0530

    Loops

--- Amstrong Number between Intervals/amstrong_number_between_intervals.c
+++ Amstrong Number between Intervals/amstrong_number_between_intervals.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+
 int amstrong_number(int n)
 {
     int i,count=0,temp=n,res=0;
@@ -23,10 +24,17 @@ int amstrong_number(int n)
 int main()
 {
     int i,a,b;
+
+    printf("\nEnter an interval to find Amstrong Numbers Between them : ");
     scanf("%d %d\n",&a,&b);
+    
+    printf("Amstrong numbers between %d and %d are/is ",a,b);
     for(i=a;i<=b;i++)
     {
         amstrong_number(i);
     }
+    
+    printf("\n\n");
+
     return 0;
 }
