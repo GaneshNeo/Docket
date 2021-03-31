49180f9bc1047159411f0d70bac773a7e22bdba6
GaneshNeo <1325982ganesh@gmail.com>
2021-03-31 13:32:57 +0530

    Binary

--- Given binary value is set or not/set_or_not_binary.c
+++ Given binary value is set or not/set_or_not_binary.c
@@ -5,12 +5,16 @@ int main()
 {
     int i,a[10],n,res;
     char str[10];
+
     printf("\n\nEnter a Binary value : ");
     scanf("%s",str);
     printf("Enter the bit position to find it is set or not :");
     scanf("%d",&n);
+
     i=strlen(str);
+    
     res = str[i-n] - '0';
+    
     if(res == 1)
     {
         printf("%d position Bit is set in the given binary number %s\n\n",n,str);
@@ -18,5 +22,6 @@ int main()
     else {
         printf("%d Bit is not set in the given binary number %s\n\n",n,str);
     }
+    
     return 0;
 }
